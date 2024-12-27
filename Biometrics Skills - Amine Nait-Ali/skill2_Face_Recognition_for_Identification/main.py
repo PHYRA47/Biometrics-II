a2e75be3701c582ce7345cad082736d1f5632745
PHYRA47 <fromsateshome47@outlook.com>
2024-12-27 03:53:40 +0100

    Suppress TensorFlow logging by setting environment variable

--- Biometrics Skills - Amine Nait-Ali/skill2_Face_Recognition_for_Identification/main.py
+++ Biometrics Skills - Amine Nait-Ali/skill2_Face_Recognition_for_Identification/main.py
@@ -1,4 +1,6 @@
 import sys
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 from PyQt5.QtWidgets import QApplication
 # from src.app_face_recognition import FaceRecognitionApp
 from src.app_DeepFace import FaceRecognitionApp
